b508fa00ed9b1cb9e5a57b9e9e3b47bf4fbb7f5f
im-adnangujjar <adnan.yaqoob.gujar@gmail.com>
2022-04-27 09:14:36 +0100

    blog

---
+++ pages/blog.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FirstSection from "../src/component/userWeb/firstSection/FirstSection";
+import SecondSection from '../src/component/userWeb/secondSection/SecondSection';
+import Tabs from "../src/commonComponent/tabs/Tabs";
+
+function blog() {
+  return (
+    <div>
+       <Tabs/>
+    </div>
+  )
+}
+
+export default blog
---
+++ src/commonComponent/card/Card.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from "../../../styles/Card.module.css";
+
+
+function Card({ Cards }) {
+    return (
+        <div className={styles.display}>
+            <div>
+                <img className={styles.image} src={Cards.image} alt="image not found" />
+            </div>
+            <div className={styles.cardItem}>
+                <h1 className={styles.dateHeading}>{Cards.date} </h1>
+                <p className={styles.description}> {Cards.description}</p>
+                <div className={styles.display1}>
+                <p className={styles.read}>{Cards.read}</p>
+                <img  className={styles.icon}src={Cards.icon} alt="icon not found" />
+                </div>
+               
+             </div>
+        </div>
+    )
+}
+
+export default Card
